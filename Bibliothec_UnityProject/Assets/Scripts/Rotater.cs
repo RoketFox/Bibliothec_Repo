237eef2a50cbe561d3c1203852aa882b2df6f755
Max <maksim.dmd05@gmail.com>
2021-03-29 20:23:12 +0300

    Working Player rotater

---
+++ Bibliothec_UnityProject/Assets/Scripts/Rotater.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    public Transform[] GroundChecks;
+    public LayerMask GroundLayers;
+    Vector3 GroundDir = Vector3.zero;
+    public float turnSpeed = 2f;
+    public float GravityRotationSpeed = 10f;
+
+    private void Awake()
+    {
+        GroundDir = transform.up;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 SetGroundDir = FloorAngleCheck();
+        GroundDir = Vector3.Lerp(GroundDir, SetGroundDir, Time.deltaTime * GravityRotationSpeed);
+
+        //lerp mesh slower when not on ground
+        RotateSelf(SetGroundDir, Time.deltaTime, GravityRotationSpeed);
+        RotateMesh(Time.deltaTime, transform.forward, turnSpeed);
+    }
+
+    Vector3 FloorAngleCheck()
+    {
+        RaycastHit HitFront;
+        RaycastHit HitCentre;
+        RaycastHit HitBack;
+
+        Physics.Raycast(GroundChecks[0].position, -GroundChecks[0].transform.up, out HitFront, 10f, GroundLayers);
+        Physics.Raycast(GroundChecks[1].position, -GroundChecks[1].transform.up, out HitCentre, 10f, GroundLayers);
+        Physics.Raycast(GroundChecks[2].position, -GroundChecks[2].transform.up, out HitBack, 10f, GroundLayers);
+
+        Vector3 HitDir = transform.up;
+
+        if (HitFront.transform != null)
+        {
+            HitDir += HitFront.normal;
+        }
+        if (HitCentre.transform != null)
+        {
+            HitDir += HitCentre.normal;
+        }
+        if (HitBack.transform != null)
+        {
+            HitDir += HitBack.normal;
+        }
+
+        Debug.DrawLine(transform.position, transform.position + (HitDir.normalized * 5f), Color.red);
+
+        return HitDir.normalized;
+    }
+    void RotateSelf(Vector3 Direction, float d, float GravitySpd)
+    {
+        Vector3 LerpDir = Vector3.Lerp(transform.up, Direction, d * GravitySpd);
+        transform.rotation = Quaternion.FromToRotation(transform.up, LerpDir) * transform.rotation;
+    }
+    //rotate the direction we face forwards
+    void RotateMesh(float d, Vector3 LookDir, float spd)
+    {
+        Quaternion SlerpRot = Quaternion.LookRotation(LookDir, transform.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, SlerpRot, spd * d);
+    }
+}
